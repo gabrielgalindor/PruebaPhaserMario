6e5d353a2878086be75b6b6aee6fc8e6db64f2d3
unknown <gabrielgalindorodriguez@gmail.com>
2021-05-17 23:55:55 -0500

    first commit

---
+++ js/inicio.js
@@ -0,0 +1,42 @@
+const config ={
+	width:800,
+	height:600,
+	parent:"container",
+	type: Phaser.AUTO,
+	physics: {
+        default: 'arcade',
+        arcade: { debug: true }
+    },
+	scene:{
+		preload:preload,
+		create:create,
+		update: update,
+		debug:true
+	}
+}
+
+
+var game = new Phaser.Game(config);
+
+function preload(){
+	this.load.image("mario","./assets/mario.png");
+	this.load.image("block","./assets/block.png");
+}
+function create(){
+
+	var mario = this.physics.add.image(200, 300, 'mario').setScale(0.25);
+
+	var block = this.physics.add.staticImage(600, 300, 'block');
+
+	this.physics.accelerateToObject(mario, block, 60, 300, 300);
+
+	var collider = this.physics.add.overlap(mario, block, function (marioOnBlock)
+    {
+        marioOnBlock.body.stop();
+
+        this.physics.world.removeCollider(collider);
+    }, null, this);
+}
+function update(time,delta){
+	
+}
